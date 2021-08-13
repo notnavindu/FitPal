bf0c9f054bc3271c1f65845644a52b890538b9d9
Menake Dasunpriya <menakedasunpriya@gmail.com>
2021-08-13 22:24:48 +0530

    add: forum screens

---
+++ app/src/main/java/com/unicodedev/fitpal/ForumQuestion.java
@@ -0,0 +1,14 @@
+package com.unicodedev.fitpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForumQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum_question);
+    }
+}
--- app/src/main/java/com/unicodedev/fitpal/MainActivity.java
+++ app/src/main/java/com/unicodedev/fitpal/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSubmit2(View view) {
-        Intent i = new Intent(this, FirstVisit.class);
+        Intent i = new Intent(this, ForumQuestion.class);
         startActivity(i);
     }
 }
