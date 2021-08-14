0c6e649004a6b145defceda4cd9b5fc6bbfcab1f
Navindu <navanavi3@gmail.com>
2021-08-14 12:51:25 +0530

    add: newPost and Comments pages

--- app/src/main/java/com/unicodedev/fitpal/social/Comments.java
+++ app/src/main/java/com/unicodedev/fitpal/social/Comments.java
@@ -1,16 +1,18 @@
-package com.unicodedev.fitpal;
+package com.unicodedev.fitpal.social;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.unicodedev.fitpal.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Social extends AppCompatActivity {
+public class Comments extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_social_home);
+        setContentView(R.layout.activity_social_comments);
     }
 
     public void onSubmit(View view) {
