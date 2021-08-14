72e464d70d7321a9b601d983281f4ae65d4afbe3
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-08-15 01:15:01 +0530

    fixes: major bugs

--- app/src/main/java/com/unicodedev/fitpal/HomeActivity.java
+++ app/src/main/java/com/unicodedev/fitpal/HomeActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.unicodedev.fitpal.dietplan.DietPlanMain;
+import com.unicodedev.fitpal.dietplan.DietPlanHome;
 import com.unicodedev.fitpal.forum.ForumMain;
 import com.unicodedev.fitpal.social.SocialHome;
 import com.unicodedev.fitpal.workout.WorkoutPlan;
@@ -36,7 +36,7 @@ public class HomeActivity extends AppCompatActivity {
         dietPlanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), DietPlanMain.class);
+                Intent i = new Intent(getApplicationContext(), DietPlanHome.class);
                 startActivity(i);
             }
         });
