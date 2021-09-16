3223e6d31816a06ed0ab54c06cb49a85e2ba88f4
SankheH <shapu953@gmail.com>
2021-09-16 20:53:37 +0530

    Started creating Diet plan-main Created Diet plan-main Done creating the Diet plan-main Resolved: Warnings Resolved: Warnings and errors Resolved: Warnings and errors Added the fonts Added fat percentage calculator Minor changes to fat percentage calculator Created custom spinner for dietPlan selection

---
+++ app/src/main/java/com/unicodedev/fitpal/dietplan/IntensityItem.java
@@ -0,0 +1,13 @@
+package com.unicodedev.fitpal.dietplan;
+
+public class IntensityItem {
+    private String mIntensityItem;
+
+    public IntensityItem(String intensityItem){
+        mIntensityItem = intensityItem;
+    }
+
+    public String getIntensityItem() {
+        return mIntensityItem;
+    }
+}
--- app/src/main/java/com/unicodedev/fitpal/dietplan/SelectDietPlan.java
+++ app/src/main/java/com/unicodedev/fitpal/dietplan/SelectDietPlan.java
@@ -1,30 +1,44 @@
 package com.unicodedev.fitpal.dietplan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.unicodedev.fitpal.R;
 
-public class SelectDietPlan extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private Button addFoodBtn;
+public class SelectDietPlan extends AppCompatActivity {
+    private ArrayList<IntensityItem> mIntensityList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dietplan_select_plan);
 
-        addFoodBtn = findViewById(R.id.start_diet_btn);
-        addFoodBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), DietPlan.class);
-                startActivity(i);
-            }
+        initList();
+
+        Spinner spinnerIntensityItems = findViewById(R.id.intensity_input);
+
+        IntensityAdapter mAdapter = new IntensityAdapter(this, mIntensityList);
+        spinnerIntensityItems.setAdapter(mAdapter);
+
+
+        Button addFoodBtn = findViewById(R.id.start_diet_btn);
+        addFoodBtn.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(), DietPlan.class);
+            startActivity(i);
         });
+    }
+
+    private void initList() {
+        mIntensityList = new ArrayList<>();
+        mIntensityList.add(new IntensityItem(""));
+        mIntensityList.add(new IntensityItem("Beginner"));
+        mIntensityList.add(new IntensityItem("Moderate"));
+        mIntensityList.add(new IntensityItem("Beast"));
     }
 }
