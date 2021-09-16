ce5d522d5fef5f467a8661de25b36d4c01b39741
SankheH <shapu953@gmail.com>
2021-09-17 01:53:41 +0530

    Started creating Diet plan-main Created Diet plan-main Done creating the Diet plan-main Resolved: Warnings Resolved: Warnings and errors Resolved: Warnings and errors Added the fonts Added fat percentage calculator Minor changes to fat percentage calculator Created custom spinner for dietPlan selection Fixed warnings Minor changes Changed text color in select diet plan

--- app/src/main/java/com/unicodedev/fitpal/dietplan/SelectDietPlan.java
+++ app/src/main/java/com/unicodedev/fitpal/dietplan/SelectDietPlan.java
@@ -2,8 +2,13 @@ package com.unicodedev.fitpal.dietplan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +17,10 @@ import com.unicodedev.fitpal.R;
 import java.util.ArrayList;
 
 public class SelectDietPlan extends AppCompatActivity {
+    EditText current_weight_input;
+    EditText target_weight_input;
+    TextView diet_plan_days;
+
     private ArrayList<IntensityItem> mIntensityList;
 
     @Override
@@ -25,6 +34,19 @@ public class SelectDietPlan extends AppCompatActivity {
 
         IntensityAdapter mAdapter = new IntensityAdapter(this, mIntensityList);
         spinnerIntensityItems.setAdapter(mAdapter);
+
+        spinnerIntensityItems.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                IntensityItem clickedItem = (IntensityItem) parent.getItemAtPosition(position);
+                String clickedIntensityItem = clickedItem.getIntensityItem();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
         Button addFoodBtn = findViewById(R.id.start_diet_btn);
