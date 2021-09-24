9c32b855bfcc4291c30d138990d4b8c15bb67826
SankheH <shapu953@gmail.com>
2021-09-24 22:36:40 +0530

    Resolved warnings

---
+++ app/src/main/java/com/unicodedev/fitpal/dietplan/DietPlan.java
@@ -0,0 +1,72 @@
+package com.unicodedev.fitpal.dietplan;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.unicodedev.fitpal.R;
+
+public class DietPlan extends AppCompatActivity {
+    Button update_btn;
+    TextView calories_display, current_weight, target_weight;
+    FirebaseFirestore db;
+    FirebaseAuth fAuth;
+    String userId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_plan);
+
+        calories_display = findViewById(R.id.calories_display);
+        current_weight = findViewById(R.id.current_weight);
+        target_weight = findViewById(R.id.target_weight);
+        update_btn = findViewById(R.id.update_btn);
+
+
+        db = FirebaseFirestore.getInstance();
+        fAuth = FirebaseAuth.getInstance();
+        userId = fAuth.getCurrentUser().getUid();
+
+        db.collection("Diet-plan").whereEqualTo("userId", userId).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(error != null){
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }else{
+                    for(QueryDocumentSnapshot doc : value){
+                        calories_display.setText("Calorie goal is: " + doc.getDouble("calories").toString());
+                        current_weight.setText("Current weight is: " + doc.getDouble("currentWeight").toString());
+                        target_weight.setText("Target weight is: " + doc.getDouble("targetWeight").toString());
+                    }
+                }
+
+            }
+        });
+
+        update_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DietPlan.this, AddFood.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+}
